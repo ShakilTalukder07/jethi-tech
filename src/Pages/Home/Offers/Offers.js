489ad387cef99ee7db839d3b83593a4f581f8d1c
ShakilTalukder07 <shakil.talukder07@gmail.com>
2023-04-03 22:17:55 +0600

    banner section completed

--- src/Pages/Home/Offers/Offers.js
+++ src/Pages/Home/Offers/Offers.js
@@ -2,6 +2,7 @@ import React from 'react';
 import offer1 from '../../../assets/offer1.png';
 import offer2 from '../../../assets/offer2.png';
 import offer3 from '../../../assets/offer3.png';
+import Button from '../../../components/Buttons/Button';
 
 
 const Offers = () => {
@@ -20,21 +21,30 @@ const Offers = () => {
                     <img src={offer1} alt="" className='w-[300px] h-[210px] rounded-lg m-2 ' />
                     <div className='m-2'>
                         <h2 className='text-xl mb-2'>Background verification</h2>
-                        <p className='text-xs'>Assuring your capital is in safe hands, we provide detailed verification reports of the project and developer. These are customized and detailed...</p>
+                        <p className='text-xs mb-1'>Assuring your capital is in safe hands, we provide detailed verification reports of the project and developer. These are customized and detailed...</p>
+                        <Button>
+                            Contact Us
+                        </Button>
                     </div>
                 </div>
                 <div className='w-[467px] h-[360px] border-black border-[1px] rounded-lg '>
                     <img src={offer2} alt="" className='w-[300px] h-[210px] rounded-lg m-2 ' />
                     <div className='m-2'>
                         <h2 className='text-xl mb-2'>Virtual site visit</h2>
-                        <p className='text-xs'>It becomes imperative to observe your apartment or plot very closely before paying the booking amount. We are here to assist with an AI-enabled UAV...</p>
+                        <p className='text-xs mb-1'>It becomes imperative to observe your apartment or plot very closely before paying the booking amount. We are here to assist with an AI-enabled UAV...</p>
+                        <Button>
+                            Contact Us
+                        </Button>
                     </div>
                 </div>
                 <div className='w-[467px] h-[360px] border-black border-[1px] rounded-lg '>
                     <img src={offer3} alt="" className='w-[300px] h-[210px] rounded-lg m-2 ' />
                     <div className='m-2'>
                         <h2 className='text-xl mb-2'>Title diligence</h2>
-                        <p className='text-xs'>Due diligence is necessary to prevent real estate malpractices which have contributed to more than 20% of the civil cases in India. We have a panel of...</p>
+                        <p className='text-xs mb-1'>Due diligence is necessary to prevent real estate malpractices which have contributed to more than 20% of the civil cases in India. We have a panel of...</p>
+                        <Button>
+                            Contact Us
+                        </Button>
                     </div>
                 </div>
             </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,29 @@
 import React from 'react';
+import logo from "../../assets/logo.png"
 
 const Footer = () => {
     return (
         <div className='bg-[#002550] text-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 '>
-            <div>
-
+            <div className='mt-6 ml-10'>
+                <img className='h-10 w-10' src={logo} alt="" />
             </div>
-            <div className='flex flex-col'>
-                <h3 className='font-bold'>Company</h3>
-                <a href=".">Home</a>
-                <a href=".">Our Offerings</a>
-                <a href=".">Our Team</a>
-                <a href=".">Contact US</a>
+            <div className='flex flex-col my-6'>
+                <h3 className='font-bold text-xl'>Company</h3>
+                <a className='mt-2 text-sm' href=".">Home</a>
+                <a className='mt-2 text-sm' href=".">Our Offerings</a>
+                <a className='mt-2 text-sm' href=".">Our Team</a>
+                <a className='mt-2 text-sm' href=".">Contact US</a>
             </div>
-            <div className='flex flex-col'>
-                <h3 className='font-bold'>Useful Link</h3>
-                <a href=".">Blog</a>
-                <a href=".">FAQ's</a>
+            <div className='flex flex-col mt-6'>
+                <h3 className='font-bold text-xl'>Useful Link</h3>
+                <a className='mt-2 text-sm' href=".">Blog</a>
+                <a className='mt-2 text-sm' href=".">FAQ's</a>
             </div>
-            <div>
-
+            <div className='mt-6'>
+                <h2 className='font-bold text-xl'>Email:</h2>
+                <p className='text-sm' >support@inreglobal.com</p>
+                <h2 className='font-bold text-xl'>Phone:</h2>
+                <p className='text-sm' >+91 | 7019305889</p>
             </div>
         </div>
     );
